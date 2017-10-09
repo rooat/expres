55ea1aa1c42aba2201468d16ac41eb42fac4ac09
rahul703 <703rahul.singh@gmail.com>
2017-10-09 16:26:50 +0530

    modifications for server

--- webipc.js
+++ webipc.js
@@ -7,7 +7,7 @@ if (typeof web3 !== 'undefined')
     web3 = new Web3(web3.currentProvider);
 } 
 else {
-    web3 = new Web3(new Web3.providers.IpcProvider("/Users/rahul703/Library/Ethereum/geth.ipc",client));
+    // web3 = new Web3(new Web3.providers.IpcProvider("/Users/rahul703/Library/Ethereum/geth.ipc",client));
     web3 = new Web3(new Web3.providers.IpcProvider("/home/ubuntu/rinkebyChain/geth.ipc",client));
 }
 
